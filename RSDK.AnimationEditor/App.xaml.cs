5df3f1abe77b5b2ea5d653805ed7b879e8cc5266
Jd ⠀ <Jd1.ext4@gmail.com>
2022-10-30 14:02:46 -0400

    ok so will it work or

--- RSDK.AnimationEditor/App.xaml.cs
+++ RSDK.AnimationEditor/App.xaml.cs
@@ -4,7 +4,7 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using RSDK.AnimationEditor.Views;
 using System;
-using System.Threading.Tasks;
+using Windows.UI.Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,7 +24,6 @@ namespace RSDK.AnimationEditor
         public App()
         {
             this.InitializeComponent();
-            Windows.ApplicationModel.Core.CoreApplication.EnablePrelaunch(true);
         }
 
         /// <summary>
@@ -34,60 +33,22 @@ namespace RSDK.AnimationEditor
         /// <param name="args">Details about the launch request and process.</param>
         protected async override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            /* 
-
-
-
-
-
-
-
-             window.Content = new SplashContent();
-
-             //Frame rootFrame = window.Content as Frame;
-
-
-             */
-
-
             Views.Window window = new Views.Window();
             window.ExtendsContentIntoTitleBar = true;
             Frame rootFrame = window.Content as Frame;
-
-
 
             if (rootFrame == null)
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-
                 rootFrame.NavigationFailed += OnNavigationFailed;
                 window.Content = rootFrame;
+
             }
             if (rootFrame.Content == null)
             {
-                //Temporary, staying until I find a way to
-                //lower load times
-                rootFrame.Navigate(typeof(SplashContent));
                 window.Activate();
-
-
-                await Task.Delay(100);
-                rootFrame.Navigate(typeof(Views.MainPage));
-
-
-
-
-                /*var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-                Microsoft.UI.WindowId W32WindowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
-                AppWindow Win32Window = AppWindow.GetFromWindowId(W32WindowId);
-                DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(W32WindowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
-
-                var StartPos = Win32Window.Position;
-                StartPos.X = ((displayArea.WorkArea.Width - Win32Window.Size.Width) / 2);
-                StartPos.Y = ((displayArea.WorkArea.Height - Win32Window.Size.Height) / 2);
-                Win32Window.Move(StartPos);*/
-
+                rootFrame.Navigate(typeof(Views.MainPage)); 
             }
         }
 
@@ -99,6 +60,19 @@ namespace RSDK.AnimationEditor
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void CenterWindow()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(acwindow);
+            Microsoft.UI.WindowId W32WindowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow Win32Window = AppWindow.GetFromWindowId(W32WindowId);
+            DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(W32WindowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+
+            var StartPos = Win32Window.Position;
+            StartPos.X = ((displayArea.WorkArea.Width - Win32Window.Size.Width) / 2);
+            StartPos.Y = ((displayArea.WorkArea.Height - Win32Window.Size.Height) / 2);
+            Win32Window.Move(StartPos);
         }
     }
 }
--- RSDK.AnimationEditor/Views/MainPage.xaml.cs
+++ RSDK.AnimationEditor/Views/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using System;
+using System.IO;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,6 +17,7 @@ namespace RSDK.AnimationEditor.Views
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MainViewModel ViewModel => (MainViewModel)DataContext;
         public static Grid MainTitleBar { get; set; }
         public MainPage()
         {
@@ -38,9 +43,182 @@ namespace RSDK.AnimationEditor.Views
             }
         }
 
+        #region Menubar
+
+        private async void FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            //var fd = Xe.Tools.Wpf.Dialogs.FileDialog.Factory(this,
+            //    Xe.Tools.Wpf.Dialogs.FileDialog.Behavior.Open,
+            //    Xe.Tools.Wpf.Dialogs.FileDialog.Type.Any, false);
+            //if (fd.ShowDialog() == true)
+            //{
+            //    ViewModel.FileOpen(fd.FileName);
+            //}
+
+            FileOpenPicker Picker = new();
+            Picker.FileTypeFilter.Add(".ani");
+            Picker.FileTypeFilter.Add(".bin");
+            IntPtr hwnd = WindowNative.GetWindowHandle(Window.XamlWindow);
+            InitializeWithWindow.Initialize(Picker, hwnd);
+            Windows.Storage.StorageFile File = await Picker.PickSingleFileAsync();
+            if (File != null)
+            {
+                try
+                {
+                    //Load file
+                    ViewModel.FileOpen(File.Path);
+                    FindName("Column0");
+                    FindName("Column1");
+                    FindName("Column2");
+                }
+                catch (Exception ex)
+                {
+                    ContentDialog Dialog = new ContentDialog();
+
+                    Dialog.XamlRoot = XamlRoot;
+                    Dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+                    Dialog.Title = "Something went wrong";
+                    Dialog.Content = "There was a problem loading this file. " + ex.Message;
+                    Dialog.PrimaryButtonText = "Close";
+                    var result = await Dialog.ShowAsync();
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        OpenFromDialog();
+                        UnloadObject(Dialog);
+                    }
+                };
+            }
+            else
+            {
+                //No file selected, do nothing
+            }
+        }
+
+        private void FileSave_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.FileSave();
+        }
+
+        /*private void FileSaveAs_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var fd = Xe.Tools.Wpf.Dialogs.FileDialog.Factory(this,
+                Xe.Tools.Wpf.Dialogs.FileDialog.Behavior.Save,
+                Xe.Tools.Wpf.Dialogs.FileDialog.Type.Any, false);
+            if (fd.ShowDialog() == true)
+            {
+                ViewModel.FileSave(fd.FileName);
+            }
+        }*/
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Window.XamlWindow.Close();
+        }
+
+        private void ViewHitbox_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.isHitboxVX)
+            {
+                new HitboxManagerVX(ViewModel).Activate();
+            }
+            else if (ViewModel.IsHitboxV5)
+            {
+                //new Hitbox5Window(ViewModel).Show();
+            }
+        }
+
+        private void ViewTexture_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.IsAnimationDataLoaded)
+            {
+                var basePath = Path.Combine(Path.GetDirectoryName(ViewModel.FileName), ViewModel.PathMod);
+                var Window = new TextureManager(ViewModel, basePath);
+
+                Window.Activate();
+
+            }
+        }
+
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Settings));
         }
+
+        #endregion
+
+        #region Misc
+
+        private async void OpenFromDialog()
+        {
+            //var fd = Xe.Tools.Wpf.Dialogs.FileDialog.Factory(this,
+            //    Xe.Tools.Wpf.Dialogs.FileDialog.Behavior.Open,
+            //    Xe.Tools.Wpf.Dialogs.FileDialog.Type.Any, false);
+            //if (fd.ShowDialog() == true)
+            //{
+            //    ViewModel.FileOpen(fd.FileName);
+            //}
+
+            FileOpenPicker Picker = new();
+            Picker.FileTypeFilter.Add(".ani");
+            Picker.FileTypeFilter.Add(".bin");
+            IntPtr hwnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(Picker, hwnd);
+            Windows.Storage.StorageFile File = await Picker.PickSingleFileAsync();
+            if (File != null)
+            {
+                try
+                {
+                    //Load file
+                    ViewModel.FileOpen(File.Path);
+                }
+                catch (Exception ex)
+                {
+                    ContentDialog Dialog = new ContentDialog();
+
+                    Dialog.XamlRoot = XamlRoot;
+                    Dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+                    Dialog.Title = "Something went wrong";
+                    Dialog.Content = "There was a problem loading this file. " + ex.Message;
+                    Dialog.PrimaryButtonText = "Close";
+                    var result = await Dialog.ShowAsync();
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        OpenFromDialog();
+                        UnloadObject(Dialog);
+                    }
+                };
+            }
+            else
+            {
+                try
+                {
+                    /*
+                    Incase the user clicks cancel
+                    on the file dialog, wait for
+                    them to open a file
+                    */
+                    ViewModel.FileOpen(File.Path);
+                }
+                catch (Exception ex)
+                {
+                    ContentDialog Dialog = new ContentDialog();
+
+                    Dialog.XamlRoot = XamlRoot;
+                    Dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+                    Dialog.Title = "Something went wrong";
+                    Dialog.Content = "There was a problem loading this file. " + ex.Message;
+                    Dialog.PrimaryButtonText = "Close";
+                    var result = await Dialog.ShowAsync();
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        OpenFromDialog();
+                        UnloadObject(Dialog);
+                    }
+                };
+            }
+        }
+
+        #endregion
+
     }
 }
